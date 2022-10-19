646e846c9f8d44248d2f94d861b428065e04e81a
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-10-19 10:55:52 -0300

    add dotenv and pm2

--- src/configs/auth.js
+++ src/configs/auth.js
@@ -1,7 +1,7 @@
 //arquivo de configurações de autenticação
 module.exports = { //exportando o objeto
   jwt: {
-    secret: "default", //isso é a chave secreta, utilizado para gerar o token
+    secret: process.env.AUTH_SECRET || "default", //isso é a chave secreta, utilizado para gerar o token
     expiresIn: "1d" //tempo de expiração do token
   }
 }
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require("dotenv/config") //importando o dotenv pra ter acesso as variáveis de ambiente
 require("express-async-errors"); //importação da biblioteca que lida com erros
 const migrationsRun = require("./database/sqlite/migrations") //importando o banco de dados
 const AppError = require("./utils/AppError") //vamos precisar desse AppError aqui nesse arquivo
@@ -20,12 +21,12 @@ app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER)) //vamos usar um m
 app.use(routes); //expondo as rotas - aplicação vai usar essas rotas
 
 app.use((error, request, response, next) => { //extraindo os parametros que vamos precisar pra mostrar o erro
-  if(error instanceof AppError) {//erro que é gerado do lado do cliente
-    return response.status(error.statusCode).json({status: "error", message: error.message})
+  if (error instanceof AppError) {//erro que é gerado do lado do cliente
+    return response.status(error.statusCode).json({ status: "error", message: error.message })
   }
-  return response.status(500).json({status: "error", message: "Internal server error"}) //se caso o error for no servidor, se no if acima for falso
+  return response.status(500).json({ status: "error", message: "Internal server error" }) //se caso o error for no servidor, se no if acima for falso
 })
 
-const PORT = 3333; //saying which port, which address it will wait for requests, requests and return responses
+const PORT = process.env.PORT || 3333; //saying which port, which address it will wait for requests, requests and return responses
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`)); //listen - keep watching, listening to the door - the function is to say what will be done when the application starts
